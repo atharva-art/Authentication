f2a6ea5163f2778f26f8141243092f2eaff32a28
atharva-art <59384466+atharva-art@users.noreply.github.com>
2023-06-14 13:14:59 +0530

    1 file modified

--- Frontend/authentication/src/App.jsx
+++ Frontend/authentication/src/App.jsx
@@ -6,7 +6,15 @@ function App() {
 
   return (
     <>
-      <h1>Hello World</h1>
+      <h1>Login</h1>
+      <br/>
+      <label for="email">Email ID : </label>
+      <input type="email" id="email" name="email" />
+      <br/><br/>
+      <label for="password">Password : </label>
+      <input type="password" id="password" name="password" />
+      <br/><br/>
+      <button>Login</button>
     </>
   )
 }
